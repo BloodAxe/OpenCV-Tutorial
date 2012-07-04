0769535bed6fa8e0a609b454cfd05472011feda7
BloodAxe <ekhvedchenya@gmail.com>
2012-07-04 16:25:19 +0300

    Grayscale conversion is not made with ARM NEON code to improve the performace

--- OpenCV Tutorial/SampleBase.cpp
+++ OpenCV Tutorial/SampleBase.cpp
@@ -22,7 +22,7 @@ const std::vector<SampleOption>& SampleBase::getOptions()
 void SampleBase::registerOption(std::string name, bool  * value)
 {
   SampleOption opt;
-
+  
   opt.kind = OptionKindBoolean;
   opt.ptr  = value;
   
@@ -54,13 +54,65 @@ bool SampleBase::hasIcon() const
   return false == getSampleIcon().empty();
 }
 
+#if (!TARGET_IPHONE_SIMULATOR)
+namespace neon
+{
+  static void neon_asm_convert(uint8_t * __restrict dest, uint8_t * __restrict src, int numPixels)
+  {
+    __asm__ volatile("lsr          %2, %2, #3      \n"
+                     "# build the three constants: \n"
+                     "mov         r4, #28          \n" // Blue channel multiplier
+                     "mov         r5, #151         \n" // Green channel multiplier
+                     "mov         r6, #77          \n" // Red channel multiplier
+                     "vdup.8      d4, r4           \n"
+                     "vdup.8      d5, r5           \n"
+                     "vdup.8      d6, r6           \n"
+                     "0:						   \n"
+                     "# load 8 pixels:             \n"
+                     "vld4.8      {d0-d3}, [%1]!   \n"
+                     "# do the weight average:     \n"
+                     "vmull.u8    q7, d0, d4       \n"
+                     "vmlal.u8    q7, d1, d5       \n"
+                     "vmlal.u8    q7, d2, d6       \n"
+                     "# shift and store:           \n"
+                     "vshrn.u16   d7, q7, #8       \n" // Divide q3 by 256 and store in the d7
+                     "vst1.8      {d7}, [%0]!      \n"
+                     "subs        %2, %2, #1       \n" // Decrement iteration count
+                     "bne         0b            \n" // Repeat unil iteration count is not zero
+                     :
+                     : "r"(dest), "r"(src), "r"(numPixels)
+                     : "r4", "r5", "r6"
+                     );
+  }
+  
+  static void cvtBgra2Gray(const cv::Mat& input, cv::Mat& gray)
+  {
+    if (gray.rows != input.rows || gray.cols != input.cols || gray.type() != CV_8UC1)
+    {
+      gray.create(input.rows, input.cols, CV_8UC1);
+    }
+    
+    uint8_t * __restrict dest = gray.data;
+    uint8_t * __restrict src  = input.data;
+    int numPixels = input.rows * input.cols;
+    
+    neon_asm_convert(dest, src, numPixels);
+  }
+}
+#endif
+
 void SampleBase::getGray(const cv::Mat& input, cv::Mat& gray)
 {
   const int numChannes = input.channels();
   
   if (numChannes == 4)
   {
-    cv::cvtColor(input, gray, CV_BGRA2GRAY);
+#if TARGET_IPHONE_SIMULATOR
+    cv::cvtColor(input, gray, CV_BGRA2GRAY);    
+#else
+    neon::cvtBgra2Gray(input, gray);
+#endif
+    
   }
   else if (numChannes == 2)
   {
