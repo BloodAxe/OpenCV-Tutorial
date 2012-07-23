ba1d9b7b77906b411424184704b3321a7517cb94
BloodAxe <ekhvedchenya@gmail.com>
2012-07-23 16:58:35 +0300

    Old comments removal

--- OpenCV Tutorial/CartoonFilter.cpp
+++ OpenCV Tutorial/CartoonFilter.cpp
@@ -29,15 +29,9 @@ std::string CartoonFilter::getDescription() const
 
 bool CartoonFilter::processFrame(const cv::Mat& inputFrame, cv::Mat& outputFrame)
 {
-  // Pseudocode
-  // 1. Run Canny or Sobel + Threshold to get binary edges
-  // 1.1 Dilate edge image to "grow" edges
-  // 2. Perform watershed segmentation to find connected regions
-  // 3. Reduce the color by finding average color for each region and assigning it to whole region
-  // 4. Or use http://stackoverflow.com/questions/3954484/cartoonizing-real-images technique
-  
   cv::cvtColor(inputFrame, bgr, CV_BGRA2BGR);
   cv::pyrMeanShiftFiltering(bgr.clone(), bgr, sp, sr);
+
   getGray(bgr, gray);
   cv::Canny(gray, edges, 150, 150);
   
