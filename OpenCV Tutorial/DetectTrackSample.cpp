4a3b367610bdf2fd9d8d61ad19f170646bab3fe5
Anton Belodedenko <anton.belodedenko@gmail.com>
2012-07-27 10:29:10 +0100

    Completed ObjectTrackingSample and DetectTrackSample samples

---
+++ OpenCV Tutorial/DetectTrackSample.cpp
@@ -0,0 +1,9 @@
+//
+//  DetectTrackSample.cpp
+//  OpenCV Tutorial
+//
+//  Created by Anton Belodedenko on 27/07/2012.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#include <iostream>
---
+++ OpenCV Tutorial/DetectTrackSample.h
@@ -0,0 +1,14 @@
+//
+//  DetectTrackSample.h
+//  OpenCV Tutorial
+//
+//  Created by Anton Belodedenko on 27/07/2012.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#ifndef OpenCV_Tutorial_DetectTrackSample_h
+#define OpenCV_Tutorial_DetectTrackSample_h
+
+
+
+#endif
