fac79b60d67a57e0920fd70b23bbba3728707523
BloodAxe <ekhvedchenya@gmail.com>
2012-07-30 14:09:51 +0300

    Project update

--- OpenCV Tutorial/AppDelegate.mm
+++ OpenCV Tutorial/AppDelegate.mm
@@ -12,6 +12,9 @@
 #import "ImageFiltersSample.h"
 #import "CartoonFilter.h"
 #import "VideoTracking.hpp"
+#import "FeatureDetectionSample.h"
+#import "ObjectTrackingSample.h"
+#import "DetectTrackSample.h"
 
 @implementation AppDelegate
 
@@ -19,49 +22,51 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
-  
   allSamples.push_back( new EdgeDetectionSample());
   allSamples.push_back( new ImageFiltersSample());
   allSamples.push_back( new ContourDetectionSample() );
   allSamples.push_back( new CartoonFilter() );
   allSamples.push_back( new VideoTrackingSample() );
 
-  
+    allSamples.push_back( new FeatureDetectionSample() );
+    allSamples.push_back( new ObjectTrackingSample() );
+    allSamples.push_back( new DetectTrackSample() );
+    
     // Override point for customization after application launch.
-  if ([[UIDevice currentDevice] userInterfaceIdiom] == UIUserInterfaceIdiomPad)
-  {
-      UISplitViewController *splitViewController = (UISplitViewController *)self.window.rootViewController;
-      UINavigationController *navigationController = [splitViewController.viewControllers lastObject];
-      splitViewController.delegate = (id)navigationController.topViewController;
-  }
+    if ([[UIDevice currentDevice] userInterfaceIdiom] == UIUserInterfaceIdiomPad)
+    {
+        UISplitViewController *splitViewController = (UISplitViewController *)self.window.rootViewController;
+        UINavigationController *navigationController = [splitViewController.viewControllers lastObject];
+        splitViewController.delegate = (id)navigationController.topViewController;
+    }
     return YES;
 }
-							
+
 - (void)applicationWillResignActive:(UIApplication *)application
 {
-  // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
-  // Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
+    // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
+    // Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
 }
 
 - (void)applicationDidEnterBackground:(UIApplication *)application
 {
-  // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later. 
-  // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
+    // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later. 
+    // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
 }
 
 - (void)applicationWillEnterForeground:(UIApplication *)application
 {
-  // Called as part of the transition from the background to the inactive state; here you can undo many of the changes made on entering the background.
+    // Called as part of the transition from the background to the inactive state; here you can undo many of the changes made on entering the background.
 }
 
 - (void)applicationDidBecomeActive:(UIApplication *)application
 {
-  // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
+    // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
 }
 
 - (void)applicationWillTerminate:(UIApplication *)application
 {
-  // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
+    // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
 }
 
 @end
